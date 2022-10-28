f3b568fca607169c6c742276b7bfaf4c1626fa6d
Dylan Hu <git@dylan.hu>
2022-10-28 15:23:21 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import bakeryData from "./assets/bakery-data.json";
 
 function App() {
+  // TODO: use useState to create a state variable to hold the state of the cart
+  /* add your cart state code here */
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
+
+      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
+        <p>Bakery Item {index}</p> // replace with BakeryItem component
+      ))}
+
+      <div>
+        <h2>Cart</h2>
+        {/* TODO: render a list of items in the cart */}
+      </div>
     </div>
   );
 }
