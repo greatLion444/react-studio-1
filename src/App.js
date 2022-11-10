5a66a6974175ba2021bef62b39195ae41ddb2128
KYen <72288527+KYen24@users.noreply.github.com>
2022-11-10 15:34:12 -0500

    finished up web

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from "./components/BakeryItem";
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -8,24 +9,38 @@ bakeryData.forEach((item) => {
 });
 /* ############################################################## */
 
+
+
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
-
+  const [items, setItems] = useState([])
+  const [sum, totalSum] = useState(0)
   return (
     <div className="App">
-      <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
 
+        <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
+      
       {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
+        <p>
+          <BakeryItem name={item.name} image={item.image} price={item.price}/>
+          <button onClick={() => {setItems([...items, item]); totalSum(sum + item.price)}}>Add To Cart</button>
+        </p> // replace with BakeryItem component
       ))}
 
       <div>
         <h2>Cart</h2>
+          {items.map((item) => (
+            <div>
+            <p>{item.name}</p>
+            </div>
+          ))}
+          <p> total =  {sum} </p>
         {/* TODO: render a list of items in the cart */}
       </div>
     </div>
   );
 }
+
 
 export default App;
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,11 @@
 // TODO: create a component that displays a single bakery item
+
+export default function BakeryItem({name, image, price}) {
+    return (
+        <div>
+            <img src={image}></img>
+            <p>{name}</p>
+            <p>{[price]}</p>
+        </div>
+    )
+}
